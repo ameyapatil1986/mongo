096ca5f78bb95b9847b2447a0c33f9c117008363
Ameya Patil <ameya.patil@mongodb.com>
2020-05-08 11:46:15 -0400

    fixing codes

---
+++ src/BasicMath/CountPrimes.java
@@ -0,0 +1,28 @@
+package BasicMath;
+
+public class CountPrimes {
+
+    public int countPrimes(int n) {
+        boolean[] composite = new boolean[n+1];
+
+        double sqrt = Math.sqrt(n);
+
+        for(int i = 2; i <= sqrt; i++){
+            if(!composite[i]) {
+                for(int j = i * i; j <= n; j = j + i){
+                    composite[j] = true;
+                }
+            }
+        }
+
+        int res = 0;
+        for(int i = 2 ; i < composite.length-1; i++){
+            if(!composite[i]) {
+                res++;
+            }
+        }
+
+        return res;
+    }
+
+}
